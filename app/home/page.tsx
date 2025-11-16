ad4606bf4319783d4eb815bc94bbaa7ddbb36fcd
Kbgjtn <kambingjan31@gmail.com>
2025-11-16 15:58:24 +0000

    feat(page): implement post creation

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,33 +1,41 @@
+"use client";
+
+import { usePosterContext } from "@/components/post/context";
 import { cn } from "@/components/shared";
+import { useState } from "react";
 
-{
-  /* Main */
-}
+import UploadForm from "@/components/image/form";
+import PosterCard from "@/components/post/card";
+
+const DEFAULT_PAGE_SIZE = 10;
+
 export default function Home() {
+  const poster = usePosterContext();
+  const [page, _] = useState<number>(1);
+  const posters = poster.get(page, DEFAULT_PAGE_SIZE);
+
   return (
-    <div className="px-4 mx-2 sm:ml-24 space-y-4 h-svh w-full overflow-y-scroll">
-      <div
-        className={cn(
-          "sticky top-0 z-90 p-4 h-12",
-          "border border-neutral-700 border-dashed",
-          "bg-black/25 rounded-b-xs",
-          "backdrop-blur-xl",
-        )}
-      >
-        <p className="text-center">N</p>
-      </div>
+    <>
+      <div className="px-4 mt-12 space-y-4 w-full md:max-w-prose md:mx-auto overflow-y-scroll">
+        <div className="rounded border border-dashed border-transparent p-4 mt-4">
+          <UploadForm />
+        </div>
 
-      <div className="p-4 rounded border h-1/3 border-red-300 border-dashed">
-        Post Cretion Tools
-      </div>
+        <div
+          className={cn(
+            "p-4",
+            "border-l border-r border-t border-transparent border-dashed rounded",
+          )}
+        >
+          {posters &&
+            posters.length > 0 &&
+            posters.map((poster, i) => (
+              <div key={i}>
+                <PosterCard poster={poster} />
+              </div>
+            ))}
 
-      <div
-        className={cn(
-          "p-4",
-          "border-l border-r border-t border-neutral-400 border-dashed rounded",
-        )}
-      >
-        {new Array(100).fill(0, 0, 100).map((_, v) => {
+          {/* {new Array(10).fill(0, 0, 10).map((_, v) => {
           return (
             <div key={v}>
               <div className="grid grid-cols-3 gap-4 mb-4">
@@ -45,8 +53,9 @@ export default function Home() {
               <div className="flex items-center justify-center h-48 rounded bg-neutral-900 mb-4 border border-dashed border-neutral-800"></div>
             </div>
           );
-        })}
+        })} */}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
